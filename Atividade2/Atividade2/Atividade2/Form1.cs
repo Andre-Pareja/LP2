8d30a12a81e0e981ba65b3a56fafb67c55c0f198
Andre-Pareja <88410699+Andre-Pareja@users.noreply.github.com>
2021-09-14 13:29:21 -0300

    Add files via upload

---
+++ Atividade2/Atividade2/Atividade2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade2
+{
+    public partial class Form1 : Form
+    {
+        double Valor1, Valor2, Total;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                Valor1 = Convert.ToDouble(textBox1.Text);
+                Valor2 = Convert.ToDouble(textBox2.Text);
+
+                Total = Valor1 - Valor2;
+
+                textBox3.Text = Convert.ToString(Total);
+            }
+            else
+            {
+                MessageBox.Show("Informe valores nos campos 'Numero1' e 'Numero2'", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                Valor1 = Convert.ToDouble(textBox1.Text);
+                Valor2 = Convert.ToDouble(textBox2.Text);
+
+                Total = Valor1 * Valor2;
+
+                textBox3.Text = Convert.ToString(Total);
+            }
+            else
+            {
+                MessageBox.Show("Informe valores nos campos 'Numero1' e 'Numero2'", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+         
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                Valor1 = Convert.ToDouble(textBox1.Text);
+                Valor2 = Convert.ToDouble(textBox2.Text);
+               
+                Total = Valor1 / Valor2;
+
+                textBox3.Text = Convert.ToString(Total);
+            }
+            else
+            {
+                MessageBox.Show("Informe valores nos campos 'Numero1' e 'Numero2'", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                Valor1 = Convert.ToDouble(textBox1.Text);
+                Valor2 = Convert.ToDouble(textBox2.Text);
+
+                Total = Valor1 + Valor2;
+
+                textBox3.Text = Convert.ToString(Total);
+            }
+            else
+            {
+                MessageBox.Show("Informe valores nos campos 'Numero1' e 'Numero2'" , "Calculadora" , MessageBoxButtons.OK , MessageBoxIcon.Error);
+            }
+        }
+    }
+}
